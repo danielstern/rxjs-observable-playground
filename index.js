1204458e225b10f2522a8773401dda8683f54a30
Daniel Stern <daniel@danielstern.ca>
2018-01-17 12:51:05 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("test");
+
+window.observer = {
+    next: x=>console.log(x),
+    error: e=>console.error(e),
+    complete: ()=>console.info("Complete")
+};
+
+window.observable = new Rx.Observable(observer=>{
+    observer.next(42);
+    observer.complete();
+});
+
+window.subject = new Rx.Subject();
+
+window.behaviorSubject = new Rx.BehaviorSubject(42);
+
+window.replaySubject = new Rx.ReplaySubject(3);
+window.replaySubject.next("Lannister");
+window.replaySubject.next("Targaryen");
+
+window.interval = Rx.Observable.interval;
